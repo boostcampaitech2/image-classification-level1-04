f87a58f8e6b8cad96a273aafa9de0b4b6303d49d
KimDaeUng <eliza.dukim@gmail.com>
2021-08-19 12:57:10 +0900

    Fix torch.flatten -> torch.Tensor.flatten

--- model/model.py
+++ model/model.py
@@ -34,7 +34,7 @@ class CIFAR10Model(nn.Module):
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = x.flatten(1) # flatten all dimensions except batch
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
