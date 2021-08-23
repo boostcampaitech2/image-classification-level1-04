b3fb8d7504eda9c31c6a68b2288ce1dac934fa59
JJONSOO <pjs970616@naver.com>
2021-08-23 19:29:23 +0000

    Edit train.py

--- train.py
+++ train.py
@@ -10,7 +10,6 @@ from parse_config import ConfigParser
 from trainer import Trainer
 from utils import prepare_device
 
-
 # fix random seeds for reproducibility
 SEED = 123
 torch.manual_seed(SEED)
