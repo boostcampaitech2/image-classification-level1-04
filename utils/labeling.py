2529d42d63b2da4b68e076cc76a2f0f747da6253
wlsl8135 <gkswls8135@gamil.com>
2021-08-24 03:36:38 +0000

    multimodel/dataloader

--- utils/labeling.py
+++ utils/labeling.py
@@ -8,29 +8,52 @@ import csv
 from torch.utils.data import Dataset, DataLoader
 import matplotlib.pyplot as plt
 from torchvision import transforms
-def makeCSV(csv_path) :
+def makeCSVforAge(csv_path, csv_name) :
     df = pd.read_csv(csv_path)
-    with open('trainV3.csv', 'wt', newline='') as csvfile:
+    with open(csv_name, 'wt', newline='') as csvfile:
         maskwriter = csv.writer(csvfile)        
         images_name = ["mask1", "mask2", "mask3", "mask4", "mask5" , "normal", "incorrect_mask"]
         maskwriter.writerow(["gender", "race", "age", "path", "label"], )
         for index in range(len(df)) : 
             data = df.iloc[index]                
-            for image_name in images_name :  
-                label = 0
-                if "incorrect" in image_name :
-                    label+=6
-                elif "normal" in image_name :
-                    label+=12
-                if data["gender"] =="female":
-                    label+=3
-                if data["age"] > 60 :
-                    label+=2
+            for image_name in images_name :                
+                label=0
+                if data["age"] > 55 : #임의로 수정
+                    label=2
                 elif data["age"] >=30:
-                    label+=1
+                    label=1
                 maskwriter.writerow([data["gender"], data["race"], data["age"], data["path"]+"/"+image_name, label])
-makeCSV('../../input/data/train/train.csv')
-
+def makeCSVforMask(csv_path, csv_name) :
+    df = pd.read_csv(csv_path)
+    with open(csv_name, 'wt', newline='') as csvfile:
+        maskwriter = csv.writer(csvfile)        
+        images_name = ["mask1", "mask2", "mask3", "mask4", "mask5" , "normal", "incorrect_mask"]
+        maskwriter.writerow(["gender", "race", "age", "path", "label"], )
+        for index in range(len(df)) : 
+            data = df.iloc[index]                
+            for image_name in images_name :                
+                label=0
+                if "incorrect" in image_name :
+                    label=2
+                elif "normal" in image_name:
+                    label=1
+                maskwriter.writerow([data["gender"], data["race"], data["age"], data["path"]+"/"+image_name, label])
+def makeCSVforSex(csv_path, csv_name) :
+    df = pd.read_csv(csv_path)
+    with open(csv_name, 'wt', newline='') as csvfile:
+        maskwriter = csv.writer(csvfile)        
+        images_name = ["mask1", "mask2", "mask3", "mask4", "mask5" , "normal", "incorrect_mask"]
+        maskwriter.writerow(["gender", "race", "age", "path", "label"], )
+        for index in range(len(df)) : 
+            data = df.iloc[index]                
+            for image_name in images_name :                
+                label = 0 #남자
+                if "fe" in data["gender"] :
+                    label=1#여자
+                maskwriter.writerow([data["gender"], data["race"], data["age"], data["path"]+"/"+image_name, label])
+#makeCSVforAge('../input/data/train/train.csv', 'train_for_Age.csv')
+#makeCSVforMask('../input/data/train/train.csv', 'train_for_Mask.csv')
+#makeCSVforSex('../input/data/train/train.csv', 'train_for_Sex.csv')
 class CustomDataset(Dataset):
     def __init__(self, csv_path, images_folder, transform = None):
         self.df = pd.read_csv(csv_path)
@@ -45,8 +68,12 @@ class CustomDataset(Dataset):
             jpg_path = path+".jpg"
             image = PIL.Image.open(os.path.join(self.images_folder, jpg_path))
         except:
-            png_path = path+".png"
-            image = PIL.Image.open(os.path.join(self.images_folder, png_path))
+            try:
+                png_path = path+".png"
+                image = PIL.Image.open(os.path.join(self.images_folder, png_path))
+            except:
+                jpeg_path = path+".jpeg"
+                image = PIL.Image.open(os.path.join(self.images_folder, jpeg_path))
         if self.transform is not None:
             image = self.transform(image)        
         return image, label
@@ -54,7 +81,7 @@ transform = transforms.Compose([
     # you can add other transformations in this list
     transforms.ToTensor()
 ])
-data=CustomDataset(csv_path='../../input/data/train/trainV3.csv', images_folder='../../input/data/train/images', transform=transform)
+data=CustomDataset(csv_path='train_for_Sex.csv', images_folder='../input/data/train/images', transform=transform)
 
 dataLoader = DataLoader(dataset=data, batch_size=16, shuffle=True)
 images, labels = next(iter(dataLoader))
