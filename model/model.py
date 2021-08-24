4c9bbdbe2572b4f25f23f5780656856bd7a5bcd5
eliza.dukim <eliza.dukim@gmail.com>
2021-08-24 13:51:37 +0000

    Fix timm model

--- model/model.py
+++ model/model.py
@@ -89,6 +89,13 @@ class PretrainModelTimm(nn.Module):
         self.num_classes = num_classes
         self.model = timm.create_model(model_name, pretrained=True)
         self.model.fc = torch.nn.Linear(in_features=512, out_features=self.num_classes, bias=True)
+        
+        n_features = self.model.head.fc.in_features
+        self.model.head.fc = torch.nn.Linear(in_features=n_features, out_features=self.num_classes, bias=True)
+
+        torch.nn.init.xavier_uniform_(self.model.head.fc.weight)
+        stdv = 1/np.sqrt(self.num_classes)
+        self.model.head.fc.bias.data.uniform_(-stdv, stdv)
 
     def forward(self, x):
         return self.model(x)
@@ -99,15 +106,16 @@ class PretrainModelTimmEcaNFNet10(nn.Module):
     timm pretrained model format
     https://fastai.github.io/timmdocs/
     """
-    def __init__(self, model_name="hf_hub:timm/eca_nfnet_l0", num_classes=18):
+    def __init__(self, model_name="eca_nfnet_l0", num_classes=18):
         super().__init__()
         self.num_classes = num_classes
         self.model = timm.create_model(model_name, pretrained=True)
-        self.model.fc = torch.nn.Linear(in_features=512, out_features=self.num_classes, bias=True)
+        n_features = self.model.head.fc.in_features
+        self.model.head.fc = torch.nn.Linear(in_features=n_features, out_features=self.num_classes, bias=True)
 
-        config = self.model.default_cfg
-        img_size = config["test_input_size"][-1] if "test_input_size" in config else config["input_size"][-1]
-        print(f'input image_size: {img_size}')
+        torch.nn.init.xavier_uniform_(self.model.head.fc.weight)
+        stdv = 1/np.sqrt(self.num_classes)
+        self.model.head.fc.bias.data.uniform_(-stdv, stdv)
 
     def forward(self, x):
         return self.model(x)
