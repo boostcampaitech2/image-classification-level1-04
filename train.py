041096235fac35208ef51dc7e63742697062c3e2
Choi Han Jun <gkswns3708@naver.com>
2021-08-24 06:44:56 +0000

    test commit

--- train.py
+++ train.py
@@ -28,7 +28,7 @@ def main(config):
 
     # build model architecture, then print to console
     model = config.init_obj('arch', module_arch)
-    logger.info(model)
+    logger.info(model) 
 
     # prepare for (multi-device) GPU training
     device, device_ids = prepare_device(config['n_gpu'])
